fadb0bb6c2ebe8e9964214c483f50584bc5d260d
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-08-03 21:10:30 +0300

    Add border collision detection

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctxt = canvas.getContext("2d");
+const angleMeter = document.getElementById("angle-meter");
 
+let rad = 0;
 let balls = [];
 
 class ball {
@@ -13,32 +15,50 @@ class ball {
         this.dy = Math.sin(angle) * 6;
     }
 
+    // this function should be called inside render
     move() {
         this.x += this.dx;
         this.y += this.dy;
     }
 
+    // called in render as well
     draw() {
         ctxt.save();
         ctxt.beginPath();
         ctxt.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctxt.fillStyle = "red";
         ctxt.fill();
+        setTimeout(1000);
+    }
+}
+
+function hitBorders(ball) {
+    if(ball.x + ball.radius > canvas.width
+    || ball.x - ball.radius < 0
+    || ball.y + ball.radius > canvas.height
+    || ball.y - ball.radius < 0
+    ) {
+        return true;
     }
 }
 
 canvas.addEventListener("click", (e)=> {
-    balls.push(new ball(Math.PI/2, e.offsetX, e.offsetY));
+    balls.push(new ball(rad, e.offsetX, e.offsetY));
+});
+
+canvas.addEventListener("dblclick", (e) => {
+    rad += 0.1;
 });
 
 function render() {
     requestAnimationFrame(render);
+    // clearing; very important, otherwise it'll draw a line not a circle.
     ctxt.clearRect(0, 0, canvas.width, canvas.height);
 
     balls.forEach(ball => {
-        ball.move();
+        if(!hitBorders(ball)) {ball.move();}
         ball.draw();
-}); 
+    }); 
 }
 
 render();
