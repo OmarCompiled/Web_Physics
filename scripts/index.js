510d13232da63b506bc960a64f2103ba1e61cf11
OmarCompiled <179285620+OmarCompiled@users.noreply.github.com>
2025-08-04 07:55:06 +0300

    Add wall Collision detection and friction/damping

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ const canvas = document.querySelector("canvas");
 const ctxt = canvas.getContext("2d");
 const angleMeter = document.getElementById("angle-meter");
 
-let rad = 0;
+let rad = -Math.PI / 3;
+let g   = 0.5;
 let balls = [];
 
 class ball {
@@ -11,36 +12,66 @@ class ball {
         this.y = y;
         this.radius = 15; // arbitrary, could be changed
         this.mass = this.radius;
-        this.dx = Math.cos(angle) * 6; // velocities
-        this.dy = Math.sin(angle) * 9.8;
+        this.dx = Math.cos(angle) * 7; // velocities
+        this.dy = Math.sin(angle) * 7;
     }
 
     // this function should be called inside render
     move() {
+        this.dy += g;
         this.x += this.dx;
         this.y += this.dy;
     }
 
     // called in render as well
     draw() {
-        ctxt.save();
         ctxt.beginPath();
         ctxt.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctxt.fillStyle = "red";
+        ctxt.fillStyle = "black";
         ctxt.fill();
         setTimeout(1000);
     }
-}
 
-function hitBorders(ball) {
-    if(ball.x + ball.radius > canvas.width
-    || ball.x - ball.radius < 0
-    || ball.y + ball.radius > canvas.height
-    || ball.y - ball.radius < 0
-    ) {
-        return true;
+    wallCollision() {
+        if(this.x + this.radius > canvas.width) {
+            this.x = canvas.width - this.radius; // very important to prevent clipping
+            this.dx *= -1; // inverting velocity vector direction
+            this.dx *= 0.8; // damping/friction
+            this.dy *= 0.8; // basically velocity decreases gradually
+        } else if(this.x - this.radius < 0) {
+            this.x = this.radius; // important to allow ball to stop and prevent clipping
+            this.dx *= -1;
+            this.dx *= 0.8;
+            this.dy *= 0.8;
+        } else if(this.y + this.radius > canvas.height) {
+            this.y = canvas.height - this.radius;
+            this.dy *= -1;
+            this.dx *= 0.8;
+            this.dy *= 0.8;
+        } else if(this.y - this.radius < 0) {
+            this.y = this.radius;
+            this.dy *= -1;
+            this.dx *= 0.8;
+            this.dy *= 0.8;
+        }
+    }
+
+    ballCollision() {
+
     }
 }
+
+// function hitBorders(ball) {
+//     if(ball.x + ball.radius > canvas.width
+//     || ball.x - ball.radius < 0
+//     || ball.y + ball.radius > canvas.height
+//     || ball.y - ball.radius < 0
+//     ) {
+
+//         return true;
+//     }
+// }
+// deprecated function
 
 canvas.addEventListener("click", (e)=> {
     balls.push(new ball(rad, e.offsetX, e.offsetY));
@@ -56,11 +87,14 @@ function render() {
     ctxt.clearRect(0, 0, canvas.width, canvas.height);
 
     balls.forEach(ball => {
-        if(!hitBorders(ball)) {ball.move();}
+        ball.move(); // moving first to init dx & dy
+
+        ball.wallCollision();
+        ball.ballCollision();
+
         ball.draw();
     }); 
 
-    angleMeter.innerHTML = rad;
 }
 
 render();
