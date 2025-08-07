64771712bf9081b07b845d86da7345461475045d
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-08-07 20:53:59 +0300

    feat: implement terrible collision with vectors
    - Switched to Vec2 class for easier vector operations.
    - Created incorrect collision that is basically a downgraded version of the game of life.
    - Essentially created a flea simulator.

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
-import Ticker from "./Ticker.js";
+import Ticker from "./utils/Ticker.js";
+import Vec2 from "./utils/Vec2.js";
+import { doOverlap, overlap } from "./utils/overlap.js";
 
 const canvas = document.querySelector("canvas");
 const ctxt = canvas.getContext("2d");
-// const angleMeter = document.getElementById("angle-meter"); // redacted
 
 const gButton = document.querySelector("#g-button");
 const cButton = document.querySelector("#c-button");
@@ -29,74 +30,54 @@ const machineGunTicker = new Ticker({ freq }); // returns true freq times per se
 
 class ball {
     constructor(angle, x, y) {
-        this.x = x;
-        this.y = y;
+        this.p = new Vec2(x, y); // position vector
         this.radius = 15; // arbitrary, could be changed
         this.c = 0.8 // damping coefficient
         this.speed = 7;
         this.mass = this.radius;
-        this.dx = Math.cos(angle) * this.speed; // velocities
-        this.dy = Math.sin(angle) * this.speed;
+        this.v = (new Vec2(Math.cos(angle), Math.sin(angle))).scale(this.speed); // velocity vector
     }
 
     // this function should be called inside render
     move() {
-        this.dy += g;
-        this.x += this.dx;
-        this.y += this.dy;
+        this.v.y += g;
+        this.p.add(this.v);
     }
 
     // called in render as well
     draw() {
         ctxt.beginPath();
-        ctxt.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctxt.arc(this.p.x, this.p.y, this.radius, 0, 2 * Math.PI);
         ctxt.fillStyle = "black";
         ctxt.fill();
     }
 
     wallCollision() {
-        if (this.x + this.radius > canvas.width) {
-            this.x = canvas.width - this.radius; // very important to prevent clipping
-            this.dx *= -1; // inverting velocity vector direction
-            this.dx *= this.c; // damping/friction
-            this.dy *= this.c; // basically velocity decreases gradually
-        } else if (this.x - this.radius < 0) {
-            this.x = this.radius; // important to allow ball to stop and prevent clipping
-            this.dx *= -1;
-            this.dx *= this.c;
-            this.dy *= this.c;
-        } else if (this.y + this.radius > canvas.height) {
-            this.y = canvas.height - this.radius;
-            this.dy *= -1;
-            this.dx *= this.c;
-            this.dy *= this.c;
-        } else if (this.y - this.radius < 0) {
-            this.y = this.radius;
-            this.dy *= -1;
-            this.dx *= this.c;
-            this.dy *= this.c;
+        if (this.p.x + this.radius > canvas.width) {
+            this.p.x = canvas.width - this.radius; // very important to prevent clipping
+            this.v.x *= -1; // inverting velocity vector direction
+        } else if (this.p.x - this.radius < 0) {
+            this.p.x = this.radius; // important to allow ball to stop and prevent clipping
+            this.v.x *= -1;
+        } else if (this.p.y + this.radius > canvas.height) {
+            this.p.y = canvas.height - this.radius;
+            this.v.y *= -1;
+        } else if (this.p.y - this.radius < 0) {
+            this.p.y = this.radius;
+            this.v.y *= -1;
         }
+        else return;
+
+        this.v.scale(this.c); // damping/friction. basically velocity decreases gradually. applied in all listed cases.
     }
 }
-
-// function hitBorders(ball) {
-//     if(ball.x + ball.radius > canvas.width
-//     || ball.x - ball.radius < 0
-//     || ball.y + ball.radius > canvas.height
-//     || ball.y - ball.radius < 0
-//     ) {
-
-//         return true;
-//     }
-// }
-// deprecated function
 
 canvas.addEventListener("mousemove", (e) => {
     if (isMouseDown) setMouse(e);
 });
 
 canvas.addEventListener("mousedown", (e) => {
-    setMouse(e);
+    setMouse(e); // in case the mouse never moved before clicking
     balls.push(new ball(rad, mouseX, mouseY));
 
     isMouseDown = true;
@@ -132,6 +113,29 @@ function setMouse(e) {
     mouseY = (e.clientY - rect.top) * (canvas.height / rect.height);
 }
 
+function ballCollision() {
+    for (let i = 0; i < balls.length; i++) {
+        for (let j = i + 1; j < balls.length; j++) {
+            const ball1 = balls[i], ball2 = balls[j];
+            if (doOverlap(ball1, ball2)) {
+                // The new velocity of each ball is in the direction of the line connecting their centers.
+                // The balls are propelled in opposite directions.
+                // Will implement actual physics soon.
+                // Summary: C2 - C1 gives us vector C1C2, it's normalized, then scaled by the magnitude.
+                const mag = 4;
+                const dir = Vec2.DirTo(ball1.p, ball2.p).scale(mag);
+                const offset = Vec2.Scale(dir, ball1.radius);
+
+                ball1.p.sub(offset);
+                ball2.p.add(offset);
+
+                ball1.v.set(dir);
+                ball2.v.set(Vec2.Scale(dir, -1));
+            }
+        }
+    }
+}
+
 function render() {
     requestAnimationFrame(render);
     // clearing; very important, otherwise it'll draw a line not a circle.
@@ -141,11 +145,10 @@ function render() {
         balls.push(new ball(rad, mouseX, mouseY));
     }
 
+    balls.forEach(ball => ball.move()); // moving first to init dx & dy
+    ballCollision();
     balls.forEach(ball => {
-        ball.move(); // moving first to init dx & dy
-
         ball.wallCollision();
-
         ball.draw();
     });
 }
