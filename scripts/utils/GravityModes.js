0f46b7ee5e60a7313cb2ad216bbdfb0298968b79
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-08-08 23:01:33 +0300

    feat(grav): add init/setup for future use + chores
    - Added init/setup functions for mode-specific actions and removed set().
    - Discarded the redundant variable `g`.
    - Replaced `new Vec2(0,0)` with `Vec2.Z`.
    - Renamed GravityModeInto to GravityModeTable.

--- scripts/utils/GravityModes.js
+++ scripts/utils/GravityModes.js
@@ -1,19 +1,25 @@
 import Vec2 from "./Vec2.js";
 
 const G = 0.5e4; // gravitational constant
-let g = G; // actual gravity value being used
 
 // "Abstract" class
 class GravityMode {
     // damping
     static c = 0.85;
 
-    // set gravity
-    set() { }
+    // mode-specific actions done on switching to a new gravity mode.
+    init(ball) {
+        // generally, just set damping and acceleration.
+        ball.c = GravityMode.c;
+        ball.setAcceleration();
+    }
+    
+    // mode-specific actions done in the gameplay loop (called in index.js's update function)
+    update() {}
 
     // acceleration
-    getAcceleration(ball) {
-        return new Vec2(0, 0);
+    getAcceleration() {
+        return Vec2.Z;
     }
 
     // damping
@@ -26,10 +32,8 @@ class GravityMode {
 // Damping: yes.
 // Methods of getting acceleration: gravity.
 export class GravityModeDown extends GravityMode {
-    set() { g = G; }
-
     getAcceleration() {
-        return new Vec2(0, g);
+        return new Vec2(0, G);
     }
 
     get c() {
@@ -41,9 +45,8 @@ export class GravityModeDown extends GravityMode {
 // Damping: no.
 // Methods of getting acceleration: none.
 export class GravityModeZero extends GravityMode {
-    set() { g = 0; }
     getAcceleration() {
-        return new Vec2(0, 0);
+        return Vec2.Z;
     }
 
     get c() {
@@ -54,11 +57,9 @@ export class GravityModeZero extends GravityMode {
 // Basically "pool/billiards table" mode. Gravity is INTO the screen. Energy loss on collision.
 // Damping: yes.
 // Methods of getting acceleration: friction.
-export class GravityModeInto extends GravityMode {
-    set() { g = G; }
-
+export class GravityModeTable extends GravityMode {
     getAcceleration(ball) {
-        return Vec2.Scale(ball.v.normalized, -ball.mu * g);
+        return Vec2.Scale(ball.v.normalized, -ball.mu * G);
     }
 
     get c() {
